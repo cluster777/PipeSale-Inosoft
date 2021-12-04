445196fffbf2c4f02698c720301d906a1b6f3e3a
cluster777 <davidlaksmanasan@gmail.com>
2021-12-04 14:44:52 +0700

    enable cors on localhost:8080

--- data/API.py
+++ data/API.py
@@ -1,22 +1,21 @@
 from flask import Flask, request, jsonify, make_response
-from flask_cors import CORS
+
 import json
 
 pipe=[]
 with open('pipe.json') as f:
     pipe=json.load(f)
 app = Flask(__name__)
-app.debug = True
-CORS(app)
 
 @app.route('/api/Pipe', methods=["GET", "OPTIONS"])
 def Pipe():
     response=jsonify(pipe)
     return response
 
-@app.route("/")
-def helloWorld():
-  return "Hello, cross-origin-world!"
+@app.after_request
+def add_header(response):
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:8080'
+    return response
 
 if __name__ == '__main__':
     app.run()
